e8da6689cb3522b8330ff8a120750d63650cb66a
darahj <darah.joseph@gmail.com>
2015-05-18 14:54:30 -0400

    added Task 1

---
+++ 1.FizzBuzz/S01A1-FizzBuzz/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    
+    
+    for(int a = 1; a <= 100; a++)
+    {
+        
+        if((a % 3 == 0) && (a % 5 == 0))
+            printf("FizzBuzz\n");
+        
+        else if(a % 3 == 0)
+            printf("Fizz\n");
+        
+        else if(a % 5 == 0)
+            printf("Buzz\n");
+        
+        else if((a % 3 != 0) && (a % 5 != 0))
+            printf("%d\n",a);
+    }
+    
+    return 0;
+}
